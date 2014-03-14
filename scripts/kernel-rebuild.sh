11de01acfb143316df6d52b1668cf868111d0bfa
mike <surinmike@gmail.com>
2014-03-15 01:35:01 +0400

    modified:   scripts/kernel-rebuild.sh

--- scripts/kernel-rebuild.sh
+++ scripts/kernel-rebuild.sh
@@ -5,7 +5,7 @@ make modules_install -j4
 emerge @module-rebuild
 mount -o remount,rw /boot
 cp /usr/src/linux/arch/x86_64/boot/bzImage /boot/gentoo-kernel
+rm /boot/initramfs*
 dracut --host-only --force
-rm /boot/initramfs
 mv /boot/initramfs* /boot/initramfs
 mount -o remount,ro /boot
