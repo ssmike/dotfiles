59a799098caa8ba8611e5c46ef59cf48f58104c8
mike <surinmike@gmail.com>
2016-01-15 12:50:58 +0300

    moved stalonetray options

--- .xmonad/dzen-auto.sh
+++ .xmonad/dzen-auto.sh
@@ -13,7 +13,7 @@ pgrep conky && exit
 #take ~/.info | dzen2 -y 1000 -dock & # -y 2000
 conky -c ~/.conkyrc | /bin/sh | dzen2 -geometry +0-0 -dock -e "onstart=lower" -ta l & 
 sleep 1
-stalonetray --grow-gravity NE --window-strut bottom -bg "#000000" --kludges=fix_window_pos,use_icons_hints,force_icons_size -i 19 --icon-gravity NE --geometry 7x1-0-0 --no-shrink --window-layer normal&
+stalonetray &
 #top
 #stalonetray --grow-gravity NE --geometry 1x1-0-0 -i 20 --window-strut bottom -bg "#000000" &
 #echo "" > ~/.info
