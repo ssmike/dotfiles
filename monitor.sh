818f42253d587821ae8601b19c4dfd5d22775f60
mike <surinmike@gmail.com>
2016-07-15 11:01:49 +0300

    xrandr

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 killall conky
 killall stalonetray
-arandr
-~/.xmonad/dzen-auto.sh &
+if xrandr  | grep "HDMI3 connected"; then
+    xrandr --auto --output HDMI3  --mode 1920x1080 --right-of LVDS1 
+else
+    xrandr --auto
+fi
+xmonad --restart
