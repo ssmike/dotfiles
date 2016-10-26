5844855e9fd7ebd158d507e1b760dca09552e0dc
Mike <surinmike@gmail.com>
2016-10-26 14:17:31 +0300

    Deleted obsolete autostart enties

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -13,9 +13,7 @@ xbindkeys &
 xscreensaver -no-splash &
 yandex-disk start &
 #xautolock -locker "slock" -time 5 &
-setxkbmap us,ru 'grp:caps_toggle'
 ~/.local/share/Telegram/Telegram &
 lxpolkit &
-setxkbmap -layout us,ru -variant -option grp:caps_toggle
 #xinput set-prop 13 "Device Enabled" 0
 xsetroot -cursor_name left_ptr
