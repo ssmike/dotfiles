ab98e88156559b21209c4d8d968911c2a9030e0a
Mike <surinmike@gmail.com>
2017-02-06 10:10:51 +0300

    [xmonad] swap monitors

--- monitor.sh
+++ monitor.sh
@@ -7,7 +7,7 @@ if xrandr  | grep "DP1-1 connected"; then
     xrandr --output eDP1 --auto --output DP1-1 --auto --scale 1.3x1.3 --right-of eDP1 --panning 2496x1404+1920+0
     cp ~/.config/nitrogen/multiple.cfg ~/.config/nitrogen/bg-saved.cfg
     mkfifo ~/.info
-    tail -f -n1 ~/.info | dzen2 -geometry '+0-0' -dock -e "onstart=lower" -ta l -xs 2 &
+    tail -f -n1 ~/.info | dzen2 -geometry '+0-0' -dock -e "onstart=lower" -ta l -xs 1 &
     nitrogen --restore
     xmonad --restart
 else
