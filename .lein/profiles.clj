f0d7bd3cedac61d6961521bbaf735557af7cbfc2
Mike <surinmike@gmail.com>
2017-06-15 15:17:51 +0300

    [lein] update plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.14.0"]
-                  [lein-pprint "1.1.2"]]}}
+                  [lein-pprint "1.1.2"]
+                  [lein-checkout-deps "1.0.0"]
+                  [lein-try "0.4.3"]]}}
