20823663f6cf65571f444bec8a4fac76e0b9f6ad
Mike <surinmike@gmail.com>
2017-06-16 15:07:10 +0300

    [lein] cider-nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.14.0"]
                   [lein-pprint "1.1.2"]
                   [lein-checkout-deps "1.0.0"]
-                  [lein-try "0.4.3"]]}}
+                  [lein-try "0.4.3"]
+                  [refactor-nrepl "1.1.0"]]}}
