43e93914ec49360feba2cfda7eb31766086713bf
Mike <surinmike@gmail.com>
2017-07-24 11:59:14 +0300

    [xmonad] vk

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -13,6 +13,7 @@ xbindkeys &
 #xscreensaver -no-splash &
 yandex-disk start &
 thunderbird &
+VK &
 Telegram &
 #xautolock -locker "slock" -time 10 -detectsleep &
 /usr/libexec/polkit-gnome-authentication-agent-1 &
