b13e0ad60dc652f7c285f48cf832cf23a6947b5d
Mike <surinmike@gmail.com>
2017-09-11 18:38:09 +0300

    [xmonad] generic autostart

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 pgrep  compton && exit
 #xhost +
-redshift &
 compton -f -D 3 -b
 nitrogen --restore &
-xfce4-power-manager &
 ~/.xmonad/lightsOn.sh 120 &
-gnome-keyring-daemon &
-nm-applet &
-parcellite &
 xbindkeys &
 #xscreensaver -no-splash &
-yandex-disk start &
-thunderbird &
-Telegram &
 #xautolock -locker "slock" -time 10 -detectsleep &
 /usr/libexec/polkit-gnome-authentication-agent-1 &
 #xinput set-prop 13 "Device Enabled" 0
 
-/usr/libexec/tracker-miner-fs &
-/usr/libexec/tracker-extract &
-/usr/libexec/tracker-miner-user-guides &
-/usr/libexec/tracker-store &
+function launch(){
+python - "$@" <<EOF
+import sys
+from gi.repository import Gio
+Gio.DesktopAppInfo.new_from_filename(sys.argv[1]).launch_uris(sys.argv[2:])
+EOF
+}
+
+for file in ~/.config/autostart/*.desktop; do
+    launch $file;
+done
