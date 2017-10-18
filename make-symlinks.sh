651a1b8864103b5e54c09b743998a943253ed607
Mike <surinmike@gmail.com>
2017-10-18 13:01:20 +0300

    add force option

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -6,7 +6,7 @@ fi
 blacklist=".git . .. make-symlinks.sh"
 for file in `ls -a`; do
     if ! grep $file <<<$blacklist; then
-        if [ -e ~/$file ] && [ ! -h ~/$file ]; then
+        if [ -e ~/$file ] && [ ! -h ~/$file ] && [ $FORCE != "1" ]; then
             rm -rf $file;
             mv ~/$file .;
         else
