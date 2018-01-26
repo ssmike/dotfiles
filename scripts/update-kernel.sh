2b4bc873bda847c56a6ffe414e060c755edb9ef0
Mike <surinmike@gmail.com>
2018-01-26 12:24:03 +0300

    kernel update script

---
+++ scripts/update-kernel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /usr/src/linux
+zcat /proc/config.gz > ./.config
+make oldconfig
+make -j4
+make modules_install
+make install
+dracut -H -k `readlink /usr/src/linux | sed -e 's/linux-/\/lib\/modules\//'` -f
