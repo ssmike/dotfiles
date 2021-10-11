d12990a5e70b89b06e91e36261d626ee2b792388
Mikhail Surin <surinmike@gmail.com>
2021-10-11 14:42:53 +0300

    delete lein

--- .lein/profiles.clj
+++
@@ -1,8 +0,0 @@
-{:user {:plugins [[cider/cider-nrepl "0.18.0"]
-                  [lein-pprint "1.1.2"]
-                  [lein-checkout-deps "1.0.0"]
-                  [lein-try "0.4.3"]
-                  [refactor-nrepl "2.4.0"]
-                  [com.jakemccrary/lein-test-refresh "0.20.0"]
-                  [lein-ancient "0.6.15"]
-                  [lein-localrepo "0.5.4"]]}}
