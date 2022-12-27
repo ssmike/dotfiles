bf5edb5795c1b27210b5effbb17cca2c27772605
Mikhail Surin <surinmike@gmail.com>
2022-12-27 18:17:14 -0300

    remove srcs

--- scripts/remove-obsolete-kernels.py
+++ scripts/remove-obsolete-kernels.py
@@ -4,6 +4,7 @@ import os.path
 import argparse
 import subprocess
 import logging
+import shutil
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
 _log = logging.getLogger(__name__)
@@ -50,47 +51,93 @@ class MountedBoot:
             raise
 
 
-parser = argparse.ArgumentParser()
-# parser.add_argument('--dry-run', action='store_true')
-parser.add_argument('--no-mount', action='store_true')
-args = parser.parse_args()
+def delete_kernel_srcs(args):
+    collected_srcs = []
+    for file in os.listdir(args.base_src_path):
+        joined = os.path.join(args.base_src_path, file)
 
-base_version, base_stream = parse_version(shell(['uname', '-r'], utf8=True))
-_log.info('base version %s, %s', base_version, base_stream)
+        if not os.path.isdir(joined):
+            continue
+        if os.path.islink(joined):
+            _log.debug('skip symlink %s', joined)
+            continue
 
-with MountedBoot(not args.no_mount):
-    dirs = ['/boot', '/boot/EFI/gentoo']
-    files = []
+        if os.path.isfile(os.path.join(joined, 'Makefile')):
+            _log.debug('skip valid kernel src %s', joined)
+        else:
+            _log.info('going to delete %s', joined)
+            collected_srcs.append(joined)
 
-    for directory in dirs:
-        if os.path.exists(directory):
-            for file in os.listdir(directory):
-                fullname = os.path.join(directory, file)
-                if os.path.isfile(fullname):
-                    files.append((file, fullname))
-
-    allowed_prefixes = [
-        'vmlinuz-',
-        'config-',
-        'initramfs-',
-        'System.map-',
-    ]
-
-    collected = []
-    for fname, fullname in files:
-        for prefix in allowed_prefixes:
-            # if '-gentoo' not in fname:
-            #     continue
-            if not fname.startswith(prefix):
-                continue
-            ver, stream = parse_version(fname[len(prefix):])
-            if ver < base_version and stream == base_stream:
-                _log.info('delete %s version %s %s', fullname, ver, stream)
-                collected.append(fullname)
-            else:
-                _log.info('reject %s version %s %s', fullname, ver, stream)
+    if not collected_srcs:
+        return
 
     confirm = input('delete files [y/n] ')
     if confirm == 'y':
-        for file in collected:
-            os.unlink(file)
+        for file in collected_srcs:
+            shutil.rmtree(file)
+
+
+def remove_bootable_kernels(args):
+    kernel_ver = args.base_bootable_kernel
+    if kernel_ver is None:
+        kernel_ver = shell(['uname', '-r'], utf8=True)
+
+    base_version, base_stream = parse_version(kernel_ver)
+    _log.info('base version %s, %s', base_version, base_stream)
+
+    with MountedBoot(not args.no_mount):
+        dirs = args.bootable_dirs
+        _log.debug('assume kernel dirs %s', dirs)
+        files = []
+
+        for directory in dirs:
+            if os.path.exists(directory):
+                for file in os.listdir(directory):
+                    fullname = os.path.join(directory, file)
+                    if os.path.isfile(fullname):
+                        files.append((file, fullname))
+
+        allowed_prefixes = [
+            'vmlinuz-',
+            'config-',
+            'initramfs-',
+            'System.map-',
+        ]
+
+        collected = []
+        for fname, fullname in files:
+            for prefix in allowed_prefixes:
+                # if '-gentoo' not in fname:
+                #     continue
+                if not fname.startswith(prefix):
+                    continue
+                ver, stream = parse_version(fname[len(prefix):])
+                if ver < base_version and stream == base_stream:
+                    _log.info('delete %s version %s %s', fullname, ver, stream)
+                    collected.append(fullname)
+                else:
+                    _log.info('reject %s version %s %s', fullname, ver, stream)
+
+        if collected:
+            confirm = input('delete files [y/n] ')
+            if confirm == 'y':
+                for file in collected:
+                    os.unlink(file)
+
+
+parser = argparse.ArgumentParser()
+# parser.add_argument('--dry-run', action='store_true')
+parser.add_argument('--no-mount', action='store_true')
+parser.add_argument('--base-src-path', default='/usr/src/')
+parser.add_argument('--base-bootable-kernel', default=None)
+parser.add_argument('--bootable-dirs', action='append', default=['/boot', '/boot/EFI/gentoo'])
+
+parser.add_argument('--no-remove-kernel-srcs', default=False, action='store_true')
+parser.add_argument('--no-remove-kernels', default=False, action='store_true')
+args = parser.parse_args()
+
+if not args.no_remove_kernel_srcs:
+    delete_kernel_srcs(args)
+
+if not args.no_remove_kernels:
+    remove_bootable_kernels(args)
