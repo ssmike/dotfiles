5daadc6381882ad68cfa0d4949cd1b07fff3413a
Mikhail Surin <surinmike@gmail.com>
2023-01-15 02:10:57 -0300

    [vim] refine keybinds

--- .vim.conf.modules/start.lua
+++ .vim.conf.modules/start.lua
@@ -130,37 +130,21 @@ cmp.setup({
     --['<C-e>'] = cmp.mapping.abort(),
     ['<C-e>'] = cmp.mapping.confirm({select = true}),
 
-    ['<C-d>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    ['<C-b>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
     ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
-
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.mapping.confirm({select = true})()
-      end
+        if cmp.visible() then
+            cmp.select_next_item(select_opts)
+        elseif luasnip.jumpable(1) then
+            luasnip.jump(1)
+        else
+            fallback()
+        end
     end, {'i', 's'}),
 
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item(select_opts)
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
       else
         fallback()
       end
