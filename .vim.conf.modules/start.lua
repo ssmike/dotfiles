0c6f021bc8a6e6e81757dcedf3eccb350c3f81c9
Mikhail Surin <surinmike@gmail.com>
2023-01-15 02:19:20 -0300

    [vim] refine keybinds

--- .vim.conf.modules/start.lua
+++ .vim.conf.modules/start.lua
@@ -128,7 +128,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
 
     ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
