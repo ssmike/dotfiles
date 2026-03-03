210115e70a921d26377b7a68f23ac09779c0a2db
Mikhail Surin <surinmike@gmail.com>
2026-03-03 13:36:35 +0100

    add codelens shortcut

--- vim.conf.modules/start.lua
+++ vim.conf.modules/start.lua
@@ -52,6 +52,7 @@ local on_attach = function(client, bufnr)
 
   buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '<space>cl', '<cmd>lua vim.lsp.codelens.run()<CR>', opts)
   buf_set_keymap('n', '<space>sd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
   buf_set_keymap('n', '}d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '{d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
